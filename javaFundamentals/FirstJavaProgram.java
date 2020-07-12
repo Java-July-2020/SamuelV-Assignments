78bccecd0e412af225131d60aff44a6605a23db2
BigSamu <valdesgutierrez@gmail.com>
2020-07-12 17:54:38 -0400

    Submitting first Java assignment

---
+++ javaFundamentals/FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Samuel Valdes Gutierrez");
+        System.out.println("I am 32 years old");
+        System.out.println("My hometoen is Santiago, Chile in Southamerica");
+
+    }
+}
