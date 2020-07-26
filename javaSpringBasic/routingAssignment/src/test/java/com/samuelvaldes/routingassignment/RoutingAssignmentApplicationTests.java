aaa5e13e5bcad1dab9d1fb48f055d79089eed59c
BigSamu <valdesgutierrez@gmail.com>
2020-07-25 20:04:43 -0400

    Submitting 'Routing' assignment v1

---
+++ javaSpringBasic/routingAssignment/src/test/java/com/samuelvaldes/routingassignment/RoutingAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelvaldes.routingassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
